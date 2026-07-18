e606df4039ef0c42a0b4534dfa36596d6ac4040b
hxvdofhxll878 <trololik882002@gmail.com>
2026-07-18 13:33:26 +0500

    refactor(AIService): removed the plug

--- app/Services/AIService.php
+++ app/Services/AIService.php
@@ -11,65 +11,60 @@ class AIService
     {
         try {
 
-//            $prompt = <<<PROMPT
-//                Проанализируй комментарий пользователя.
-//                Верни только валидный JSON.
-//
-//                Доступные значения sentiment:
-//                - Positive
-//                - Neutral
-//                - Negative
-//
-//                Доступные значения category:
-//                - Question
-//                - Feedback
-//                - Complaint
-//                - Partnership
-//                - Other
-//
-//                JSON формат ответа:
-//                {
-//                    "sentiment": "",
-//                    "category": ""
-//                }
-//
-//                Комментарий пользователя:
-//                {$comment}
-//            PROMPT;
-//
-//            $response = Http::timeout(10)
-//                ->post(
-//                    'https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash:generateContent?key=' . config('services.gemini.api_key'),
-//                    [
-//                        'contents' => [
-//                            [
-//                                'parts' => [
-//                                    [
-//                                        'text' => $prompt,
-//                                    ]
-//                                ]
-//                            ]
-//                        ]
-//                    ]
-//                );
-//
-//            $data = $response->json();
-//
-//            $content = $data['candidates'][0]['content']['parts'][0]['text'];
-//
-//            $content = preg_replace('/```json\s*|\s*```/', '', $content);
-//            $content = preg_replace('/```\s*|\s*```/', '', $content);
-//
-//            $result = json_decode($content, true);
-//
-//            return [
-//                'sentiment' => $result['sentiment'] ?? 'Unknown',
-//                'category' => $result['category'] ?? 'Unknown',
-//            ];
+            $prompt = <<<PROMPT
+                Проанализируй комментарий пользователя.
+                Верни только валидный JSON.
+
+                Доступные значения sentiment:
+                - Positive
+                - Neutral
+                - Negative
+
+                Доступные значения category:
+                - Question
+                - Feedback
+                - Complaint
+                - Partnership
+                - Other
+
+                JSON формат ответа:
+                {
+                    "sentiment": "",
+                    "category": ""
+                }
+
+                Комментарий пользователя:
+                {$comment}
+            PROMPT;
+
+            $response = Http::timeout(10)
+                ->post(
+                    'https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash:generateContent?key=' . config('services.gemini.api_key'),
+                    [
+                        'contents' => [
+                            [
+                                'parts' => [
+                                    [
+                                        'text' => $prompt,
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                );
+
+            $data = $response->json();
+
+            $content = $data['candidates'][0]['content']['parts'][0]['text'];
+
+            $content = preg_replace('/```json\s*|\s*```/', '', $content);
+            $content = preg_replace('/```\s*|\s*```/', '', $content);
+
+            $result = json_decode($content, true);
 
             return [
-                'sentiment' => 'Unknown',
-                'category' => 'Unknown',
+                'sentiment' => $result['sentiment'] ?? 'Unknown',
+                'category' => $result['category'] ?? 'Unknown',
             ];
 
         } catch (\Throwable $e) {
